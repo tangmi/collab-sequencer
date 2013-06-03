9915fd6c6fb2d6c184a547159e8d6560857c2956
Michael Tang <tangmi@uw.edu>
2013-06-03 00:20:37 -0700

    Adds backend API

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-var sass = require('node-sass');
+// var sass = require('node-sass');
 var fs = require('fs');
 
 var app = express();
 
-app.configure(function () {
+app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
 	// app.set('views', __dirname + '/views');
 	// app.set('view engine', 'ejs');
@@ -19,46 +19,182 @@ app.configure(function () {
 	app.use(express.cookieParser('your secret here'));
 	app.use(express.session());
 	app.use(app.router);
-	// app.use(express.static(path.join(__dirname, 'public')));
 	app.use(express.static(path.join(__dirname, 'frontend/app')));
+
 });
 
-// var cssPath = __dirname + '/public/stylesheets/style.build.css';
-// var sassPath = __dirname + '/stylesheets/style.scss';
 
-app.configure('production', function() {
-	//build the sass file for use in production
-	fs.writeFileSync(cssPath, sass.renderSync(fs.readFileSync(sassPath), {
-		includePaths: [__dirname + '/stylesheets'],
-		outputStyle: 'compressed'
-	}), 'utf8');
-});
+app.configure('production', function() {});
 
-app.configure('development', function () {
+app.configure('development', function() {
 	app.use(express.errorHandler());
-
-	// //delete the built sass file and insert middleware to build on every request during development (slow)
-	// try {
-	// 	fs.unlinkSync(cssPath);
-	// 	console.log("Deleted built CSS file (from production build)");
-	// } catch(e) {}
-	// app.use('/stylesheets/style.build.css', function(req, res, next) {
-	// 	console.log("Rendering SASS file to CSS...");
-	// 	res.end(sass.renderSync(fs.readFileSync(sassPath), {
-	// 		includePaths: [__dirname + '/stylesheets'],
-	// 		outputStyle: 'compressed'
-	// 	}));
-	// });
 });
 
 // app.get('/', routes.index);
 
-app.get('/gus', function(req, res) {
-	res.send('hi gus');
-});
 
-http.createServer(app).listen(app.get('port'), function () {
+var data = {};
+/*
+	data.instrument.time.pitch = {
+		user, highlighted
+	}
+*/
+
+http.createServer(app).listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 
-	// console.log(sass.renderSync('', {includePaths: [__dirname + '/stylesheets']}));
+
+	var pitches = 10, //0-10
+		times = 31, //0-31
+		user = 'GUS',
+		types = ['drums', 'synth'];
+
+	for (var i in types) {
+		var type = types[i];
+		data[type] = [];
+
+		var t = 0;
+		for (t = 0; t <= times; t++) {
+			data[type][t] = [];
+
+			var p = 0;
+			for (p = 0; p <= pitches; p++) {
+				data[type][t][p] = {
+					'_toString': '#cell-' + t + '-' + p + '-' + type,
+					'user': user,
+					'highlighted': false
+				}
+			}
+		}
+	}
+
+	// console.log(dataToJson(data));
+
+});
+
+var dataToJson = function(data) {
+	var out = [];
+	var type, t, p;
+	for(type in data) {
+		for(t in data[type]) {
+			for(p in data[type][t]) {
+				var note = data[type][t][p];
+				out.push('{"pitch":' + p + ',"time":' + t + ',"user":"' + note.user + '","type":"' + type + '","highlighted":' + note.highlighted + '}');
+			}
+		}
+	}
+	return '[' + out.join(',') + ']';
+}
+
+//CORS
+app.all('/*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	// console.log("CORS");
+
+	next();
+});
+
+
+
+var redisConfig = {
+	port: 6379,
+	host: '127.0.0.1',
+	options: {}
+};
+var redis = require("redis");
+var client = redis.createClient(redisConfig.port, redisConfig.host, redisConfig.options);
+
+// client.hmset('gus', {'count': 0});
+
+app.get('/gus', function(req, res) {
+	client.hgetall("gus", function(err, obj) {
+		var count = Number(obj.count) + 1;
+		res.send('goodbye gus, #' + count);
+
+		client.hmset('gus', {
+			'count': count
+		});
+
+	});
+
+	console.log("somebody connected");
+});
+
+
+
+//testing code
+var generateBaseCollection = function(random) {
+	var pitches = 10,
+		times = 31,
+		user = 'GUS',
+		types = ['drums', ''];
+
+	var out = [];
+
+	for (var i in types) {
+		var type = types[i];
+		var t = 0;
+		for (t = 0; t <= times; t++) {
+			var p = 0;
+			for (p = 0; p <= pitches; p++) {
+				var val = random ? (Math.random() > 0.5 ? 'false' : 'true') : false;
+				out.push('{"pitch":' + p + ',"time":' + t + ',"user":"' + user + '","type":"' + type + '","highlighted":' + val + '}');
+			}
+		}
+	}
+
+	return '[' + out.join(',') + ']';
+}
+
+app.get('/reset', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(generateBaseCollection(false));
+});
+
+app.get('/reset/random', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(generateBaseCollection(true));
+});
+
+
+
+//saving
+app.post('/add', function(req, res) {
+
+	var p = req.body.pitch,
+		t = req.body.time,
+		type = req.body.type;
+
+	data[type][t][p].highlighted = req.body.highlighted;
+	data[type][t][p].user = req.body.user;
+
+	console.log("=> " + data[type][t][p].user + ": set " + data[type][t][p]._toString + " to " + data[type][t][p].highlighted);
+
+});
+
+
+
+
+app.get('/get', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(dataToJson(data));
+});
+
+
+//manual toglging of stuff
+app.get('/toggle/:type/:time/:pitch', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+
+	var p = req.params.pitch,
+		t = req.params.time,
+		type = req.params.type;
+
+	data[type][t][p].highlighted = !data[type][t][p].highlighted;
+	data[type][t][p].user = "SYSTEM";
+
+	console.log("=> " + data[type][t][p].user + ": set " + data[type][t][p]._toString + " to " + data[type][t][p].highlighted);
+
+	res.send("=> " + data[type][t][p].user + ": set " + data[type][t][p]._toString + " to " + data[type][t][p].highlighted);
 });

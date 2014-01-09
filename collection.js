2ded53f51b2345b3bdef2428f83f44cdf6c7f653
Michael Tang <tangmi@uw.edu>
2014-01-08 18:41:55 -0800

    Switch over to model-based storage rather than collection-based storage #25

--- collection.js
+++ collection.js
@@ -1,158 +1,229 @@
 var notesConfig = require('./configuration').instruments;
 
-var Storage = require('node-document-storage-fs');
-var storage = new Storage(require('./configuration').storeLocation, {});
+var path = require('path');
 
-/*
+var model = require('model');
 
-	We store the roll as time 'slices', containing some constant number of pitches
-
-	data[instrument][time][pitch] = {
-		user, highlighted
-	}
-
-*/
-exports.data = {};
-
-exports.storeCollection = function(user, cb) {
-	var callback = typeof cb === 'function' ? cb : function() {};
-
-	if (!user) {
-		user = '';
-	}
-
-	var data = JSON.stringify(exports.data);
-
-	storage.set('notes', data, function(err, results) {
-
-		console.log('=>', user + ':', 'Storing collection');
-		// console.log('=>', user + ':', 'Store collection', '\n' + exports.render(exports.data));
-
-		callback(err, results[0]);
-	});
-
+var noteProperties = {
+	pitch: {
+		type: 'number',
+		required: true
+	},
+	time: {
+		type: 'number',
+		required: true
+	},
+	user: {
+		type: 'string',
+		required: true
+	},
+	instrument: {
+		type: 'string',
+		required: true
+	},
+	highlighted: {
+		type: 'boolean',
+		required: true
+	},
 };
 
-exports.getCollection = function(user, cb) {
-	var callback = typeof cb === 'function' ? cb : function() {};
-	if (!user) {
-		user = '';
-	}
-
-	storage.get('notes', function(err, results) {
-
-		var obj = results[0];
-
-		console.log('=>', user + ':', 'Retrieved collection');
-		// console.log('=>', user + ':', 'Get collection', '\n' + exports.render(exports.data));
-
-		try {
-			exports.data = JSON.parse(obj);
-		} catch (e) {
-			exports.generateBaseCollection();
-			exports.storeCollection(user);
-		}
-
-		callback(err, obj);
+Note = model.register('Note', function() {
+	this.defineProperties(noteProperties);
+	// this.setAdapter('memory');
+	this.setAdapter('filesystem', {
+		'location': path.join(__dirname, '.model-fs'),
+		'filename': 'note'
 	});
-};
+});
 
-exports.generateBaseCollection = function() {
-	var tabs = notesConfig.tabs,
-		time = notesConfig.time,
-		pitches,
-		user = 'SYSTEM';
+function getNote(data, cb) {
+	assertNotUndefined(data.instrument, 'get needs instrument');
+	assertNotUndefined(data.time, 'get needs time');
+	assertNotUndefined(data.pitch, 'get needs pitch');
 
-	var output = {};
-
-	var i,
-		tab,
-		l = tabs.length;
-	for (i = 0; i < l; i++) {
-		tab = tabs[i];
-		console.log(tab.name);
-		output[tab.name] = {};
-
-		pitches = tab.notes.length - 1; //minus one because we are zero-indexed on the server side
-
-		for (t = 0; t <= time; t++) {
-			output[tab.name][t] = [];
-			var p = 0;
-			for (p = 0; p <= pitches; p++) {
-				output[tab.name][t][p] = {
-					'_toString': 'cell-' + tab.name + '-' + t + '-' + p, //this is purely for niceness, could be a method on collection
-					'user': user,
-					'highlighted': false
-				}
-			}
+	var query = {
+		instrument: data.instrument,
+		time: data.time,
+		pitch: data.pitch
+	};
+	Note.first(query, function(err, note) {
+		if (err) {
+			throw err;
 		}
-	}
 
-	exports.data = output;
+		cb(note);
+	});
 }
 
+function setNote(data, cb) {
+	assertNotUndefined(data.instrument, 'set needs instrument');
+	assertNotUndefined(data.time, 'set needs time');
+	assertNotUndefined(data.pitch, 'set needs pitch');
+	assertNotUndefined(data.user, 'set needs user');
+	assertNotUndefined(data.highlighted, 'set needs highlighted');
 
-var toObject = function() {
-	var data = exports.data;
-	var object = [];
-	for (type in data) {
-		for (t in data[type]) {
-			for (p in data[type][t]) {
-				var note = data[type][t][p];
-
-				object.push({
-					pitch: new Number(p),
-					time: new Number(t),
-					user: note.user,
-					type: type,
-					highlighted: note.highlighted,
-					id: note._toString
-				});
-
-			}
+	getNote(data, function(note) {
+		if (note == null) {
+			createNote(data, function(newNote) {
+				cb(newNote);
+			});
+			return;
 		}
-	}
-	return object;
+
+		note['user'] = data.user;
+		note['highlighted'] = data.highlighted;
+
+		saveNote(note, function(data) {
+			cb(data);
+		});
+	});
 }
 
-exports.getModel = function(type, time, pitch) {
+function createNote(data, cb) {
+	assertNotUndefined(data.instrument, 'create needs instrument');
+	assertNotUndefined(data.time, 'create needs time');
+	assertNotUndefined(data.pitch, 'create needs pitch');
+	assertNotUndefined(data.user, 'create needs user');
 
-	var note = exports.data[type][time][pitch];
-
-	var model = {
-		pitch: new Number(pitch),
-		time: new Number(time),
-		user: note.user,
-		type: type,
-		highlighted: note.highlighted,
-		id: note._toString
+	var input = {
+		instrument: data.instrument,
+		time: data.time,
+		pitch: data.pitch,
+		user: data.user
 	};
 
-	return model;
+	var note = Note.create(input);
 
+	saveNote(note, cb);
+}
+
+function saveNote(note, cb) {
+	note.save(function(err, data) {
+		if (err) {
+			console.log(err);
+			throw err;
+		}
+		console.log('note saved');
+		cb(data);
+	});
+}
+
+function getAllNotes(cb) {
+	Note.all({}, function(err, notes) {
+		if (err) {
+			throw err;
+		}
+		cb(notes);
+	});
+}
+
+module.exports.get = function(data, cb) {
+	console.log('GET NOTE');
+
+	getNote(data, function(note) {
+		cb(noteModelTransform(note));
+	});
+};
+module.exports.set = function(data, cb) {
+	console.log('SET NOTE');
+
+	setNote(data, function(note) {
+		cb(noteModelTransform(note));
+	});
 };
 
-exports.getData = function() {
-	return toObject();
+module.exports.getAll = function(cb) {
+	console.log('GET ALL NOTES');
+
+	getAllNotes(function(notes) {
+		notes.forEach(function (element, index, array) {
+			array[index] = noteModelTransform(array[index]);
+		});
+
+		cb(notes);
+	});
 };
 
-exports.toJson = function() {
+/*
+	helper methods
+ */
 
-	return JSON.stringify(toObject());
+function assertNotUndefined(prop, msg) {
+	if (typeof prop === 'undefined') {
+		throw new Error(msg);
+	}
+}
 
-	// var out = [];
-	// var type, t, p;
-	// for(type in this.data) {
-	// 	for(t in this.data[type]) {
-	// 		for(p in this.data[type][t]) {
-	// 			var note = this.data[type][t][p];
-	// 			out.push('{"pitch":' + p + ',"time":' + t + ',"user":"' + note.user + '","type":"' + type + '","highlighted":' + note.highlighted + '}');
-	// 		}
-	// 	}
-	// }
-	// return '[' + out.join(',') + ']';
-};
+//transforms a mde/model to an object that backbone wants
+var properties = (function() {
+	var arr = [];
+	for (var key in noteProperties) {
+		arr.push(key);
+	}
+	return arr;
+})();
 
+function noteModelTransform(note) {
+	var out = {};
+
+	for(var i = 0; i < properties.length; i++) {
+		var propertyName = properties[i];
+		out[propertyName] = note[propertyName];
+	}
+
+	return out;
+}
+
+function getEmptyNote(data) {
+	assertNotUndefined(data.instrument, 'create needs instrument');
+	assertNotUndefined(data.time, 'create needs time');
+	assertNotUndefined(data.pitch, 'create needs pitch');
+
+	var input = {
+		instrument: data.instrument,
+		time: data.time,
+		pitch: data.pitch,
+		user: 'SYSTEM',
+		highlighted: 'false'
+	};
+
+	var note = Note.create(input);
+
+	return noteModelTransform(note);
+}
+
+
+
+module.exports.set({
+	pitch: 5,
+	time: 1,
+	user: 'Michael',
+	instrument: 'drums',
+	highlighted: true
+}, function(note) {
+	console.log(note);
+});
+
+module.exports.get({
+	instrument: 'drums',
+	time: 1,
+	pitch: 5
+}, function(note) {
+	console.log(note);
+});
+
+module.exports.getAll(function(notes) {
+	console.log(notes);
+})
+
+console.log(getEmptyNote({
+	instrument: 'drums',
+	time: 1,
+	pitch: 5
+}));
+
+
+//TODO: update render method, fix API calls by storage update
 
 //renders the board as text
 exports.render = function() {
@@ -191,6 +262,6 @@ exports.render = function() {
 
 
 exports.initialize = function() {
-	exports.getCollection('SYSTEM');
+	// exports.getCollection('SYSTEM');
 	// console.log(dataToJson(data));
 };

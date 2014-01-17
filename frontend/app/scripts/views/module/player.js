4431b51cbe45f598d236c1daa8d4dbbdd737e4b8
Michael Tang <tangmi@uw.edu>
2014-01-17 15:02:03 -0800

    Moved player to modern Audio API and add dynamic compressor to prevent ears bleeding

--- frontend/app/scripts/views/module/player.js
+++ frontend/app/scripts/views/module/player.js
@@ -1,14 +1,34 @@
 define([], function() {
+
+	var context;
+	try {
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext();
+	} catch (e) {
+		alert('Web Audio API is not supported in this browser');
+	}
+
+	var compressor = context.createDynamicsCompressor();
+	// var reverb = context.createConvolver();
+	// var volume = context.createGainNode();
+
+	// compressor.connect(reverb);
+	// reverb.connect(context.destination);
+
 	var Player = {
 
 		files: {},
 
 		initialize: function(name, rows) {
+
 			for (var i in rows) {
 				if (!this.files[name]) {
 					this.files[name] = {};
 				}
-				this.files[name][rows[i].pitch] = rows[i].file;
+				this.files[name][rows[i].pitch] = {
+					url: '/sounds/' + rows[i].file,
+					buffer: null
+				};
 
 				this.load({
 					instrument: name,
@@ -18,17 +38,30 @@ define([], function() {
 		},
 
 		load: function(row) {
-			var sound = new Audio('/sounds/' + this.files[row.instrument][row.pitch]);
-			sound.preload = true;
-			sound.load();
+			var url = this.files[row.instrument][row.pitch].url;
+
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.responseType = 'arraybuffer';
+
+			var _this = this;
+			request.onload = function() {
+				context.decodeAudioData(request.response, function(buffer) {
+					_this.files[row.instrument][row.pitch].buffer = buffer;
+				}, function(err) {
+					console.err(err)
+				});
+			}
+			request.send();
 		},
 
 		playPitch: function(instrument, pitch) {
-			var audio = new Audio('/sounds/' + this.files[instrument][pitch]);
-			// audio.addEventListener("ended", function() {
-			// 	document.removeChild(this);
-			// }, false);
-			audio.play();
+			var source = context.createBufferSource();
+			source.buffer = this.files[instrument][pitch].buffer;
+			source.connect(compressor);
+			compressor.connect(context.destination);
+			// source.connect(context.destination); //destination=speakers
+			source.start(0);
 		},
 
 		play: function(rowsToPlay) {
